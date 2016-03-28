6e3f5de0bf43a0818b2ba2400f0b9c29bd7d11d5
Chris Thurber <cthurber@me.com>
2016-03-28 16:44:56 -0400

    API for gaterhing data from CBOE options data page

---
+++ CBOE_API.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from urllib import request
+from bs4 import BeautifulSoup
+
+# Grabs table from CBOE "Total options data"
+def getTotalData():
+    system_time = str(datetime.now().time()).split(":")
+    system_day = int(datetime.today().weekday())
+	
+    CBOE_options_page = BeautifulSoup(request.urlopen("http://www.cboe.com/data/intradayvol.aspx"), "html.parser")
+    table = CBOE_options_page.find("table")
+
+    table_data = table.find_all('tr')
+    table = [] #table = {}
+    for row in table_data[1:]:
+        row = str(row.text).split('\n')
+
+        time = row[1]
+        time_of_day = time.split(' ')[1]
+        hour = time.split(' ')[0].split(':')[0]
+        minute = time.split(' ')[0].split(':')[1]
+
+        calls = row[2]
+        puts = row[3]
+        total = row[4]
+        ratio = row[5]
+        
+        # Convert to military time
+        if("PM" in time_of_day and int(hour) != 12):
+            time = str(int(hour[0])+12)+":"+minute
+        else:
+            time = str(int(hour))+":"+minute
+
+		# Determine market status
+        status = ["Open","success"]
+        if(system_day > 4 or int(system_time[0]) < 9 or int(system_time[0]) >= 16):
+        	status = ["Closed","danger"]
+
+		# Add all non-null data to table
+        if(calls != '\xa0' and puts != '\xa0' and ratio != '\xa0'):
+            calls_centage = float(int(calls) / int(total))*100
+            puts_centage = float(int(puts) / int(total))*100
+            
+            table.append({"time":time,"calls":int(calls), "puts":int(puts),"call_centage":float(calls_centage),"put_centage":float(puts_centage),"ratio":float(ratio),"status":status})
+
+    return table[-1] # Get latest data
