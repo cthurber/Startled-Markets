a9c5f82bce2dd186480f4a65095409748b2d5337
Chris Thurber <chris@client-7-4-133.asb.drew.lan>
2016-03-31 23:53:56 -0400

    Todos on SP Trader

--- sp_trader.py
+++ sp_trader.py
@@ -1,6 +1,32 @@
 from stratbox_api import MarketWatch
+from CBOE_API import getLatest
+
+# Grab CBOE Data
+cboe_data = getLatest() # <- TODO fix that function in lib
 
 # Initialize MarketWatch Trading component & login
 MarketWatchDriver = MarketWatch()
 MarketWatchDriver.login()
-available_balance = MarketWatchDriver.get_balance()
+available_balance = float(MarketWatchDriver.get_balance())
+
+# ETF data
+spxs_price = MarketWatchDriver.get_price("SPXS")
+spxl_price = MarketWatchDriver.get_price("SPXL")
+
+# Step 1
+# - Determine position: Long/Short based on percentage
+spx_allocation_centage = cboe_data["ratio"]*.10
+spx_cash_alloc = available_balance*spx_allocation_centage
+spx_share_alloc = int(spx_cash_alloc / spxs_price)
+
+if(float(cboe_data["ratio"]) > 1):
+    print("buy "+str(spx_share_alloc)+" SPXS")
+elif(float(cboe_data["ratio"]) < 1):
+    print("short "+str(spx_share_alloc)+" SPXS")
+
+# - Determine magnitude: (need metric...)
+# - Determine SPXS/L weighting/allocation
+# - Allocate remaining capital*weight to SP500 companies
+
+# Step 2
+# Package {CBOE_data : dat, Portfolio : portfolio, Trades : [trades]} to be returned for webpage
